6ef7decbfb753cf587caef3cc70f0da66cf38df2
Rochet2 <tqbattlenet@gmail.com>
2015-03-06 18:03:55 +0200

    TC support and other changes for instance_scripts

--- MapMethods.h
+++ MapMethods.h
@@ -290,7 +290,15 @@ namespace LuaMap
      */
     int GetInstanceData(Eluna* E, lua_State* L, Map* map)
     {
-        if (ElunaInstanceAI* iAI = dynamic_cast<ElunaInstanceAI*>(map->GetInstanceData()))
+#ifdef TRINITY
+        ElunaInstanceAI* iAI = NULL;
+        if (InstanceMap* inst = map->ToInstanceMap())
+            iAI = dynamic_cast<ElunaInstanceAI*>(inst->GetInstanceScript());
+#else
+        ElunaInstanceAI* iAI = dynamic_cast<ElunaInstanceAI*>(map->GetInstanceData());
+#endif
+
+        if (iAI)
         {
             sEluna->PushInstanceData(iAI, false);
         }
@@ -303,9 +311,17 @@ namespace LuaMap
      */
     int SaveInstanceData(Eluna* E, lua_State* L, Map* map)
     {
-        if (InstanceData* data = map->GetInstanceData())
+#ifdef TRINITY
+        ElunaInstanceAI* iAI = NULL;
+        if (InstanceMap* inst = map->ToInstanceMap())
+            iAI = dynamic_cast<ElunaInstanceAI*>(inst->GetInstanceScript());
+#else
+        ElunaInstanceAI* iAI = dynamic_cast<ElunaInstanceAI*>(map->GetInstanceData());
+#endif
+
+        if (iAI)
         {
-            data->SaveToDB();
+            iAI->SaveToDB();
         }
 
         return 0;
